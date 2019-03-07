a5ae2dee295e7ed937c2de11b13792e8c829b874
Ben Ryan <ben96ryan@gmail.com>
2019-03-07 14:06:10 +0000

    mysql db setup, new keygen + insert working

--- Server/src/server.py
+++ Server/src/server.py
@@ -12,18 +12,6 @@ import zmq
 import base64 as b64
 import tensorflow as tf
 
-def precision(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
-
-
-def recall(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
 
 class Receiver(Thread):
 	def __init__(self, addr):
@@ -36,7 +24,7 @@ class Receiver(Thread):
 		print("Listening on", addr)
 
 	def run(self):
-		model = tf.keras.models.load_model("../../Models/model-current.h5")
+		model = tf.keras.models.load_model("../../Decent Models/model-current.h5")
 		model.summary()
 
 		while True:
