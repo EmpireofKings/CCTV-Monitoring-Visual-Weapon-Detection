1e61dcbf35a718b29b41ac8fcfaedc3f75dba332
Ben Ryan <ben96ryan@gmail.com>
2019-03-09 21:34:40 +0000

    registration and activation working, need to either setup smtp server or route through a google account to send activation key, login not yet implemented

--- Server/src/registration.py
+++ Server/src/registration.py
@@ -4,10 +4,11 @@ import time
 import uuid
 from threading import Thread
 import zmq
-
+import smtplib
+from email.message import EmailMessage
 from argon2 import PasswordHasher
 
-# import MySQLdb as sql
+import MySQLdb as sql
 from certificate_handler import CertificateHandler
 from context_handler import ContextHandler
 from terminator import Terminator
@@ -47,24 +48,27 @@ class RegistrationListener(Thread):
 			try:
 				received = self.socket.recv_string()
 				print(received)
-				parts = recevied.split(' ')
+				parts = received.split(' ')
 
 				if parts[0] == "REGISTER":
+					print("in register")
 					success = True
-					msg = 'Account Registered'
-
+					msg = ''
+					userID = 'N/A'
 					hasher = PasswordHasher()
 
 					try:
 						username = parts[1]
 						password = hasher.hash(parts[2])
 						email = parts[3]
+						print("got parts")
 					except:
 						success = False
 						msg += '\n\tError in parameters provided'
 
 					if success:
 						global cursor
+						global db
 
 						cursor.execute(
 							"""select * from users where username = %s""", (username,))
@@ -74,20 +78,21 @@ class RegistrationListener(Thread):
 							msg += '\n\tUsername already taken'
 
 						cursor.execute(
-							"""select * from users where username = %s""", (email, ))
+							"""select * from users where email = %s""", (email, ))
 
 						if cursor.rowcount != 0:
 							success = False
 							msg += '\n\tEmail already registered'
 
 						if success:
+							print("inserting")
 							count = cursor.execute(
 								"""insert into users(username, password, email)
 								values(%s, %s, %s)""", (username, password, email))
 							userID = cursor.lastrowid
 
-							cursor.commit()
-
+							db.commit()
+							print("commit")
 							if count == 1 and userID is not None:
 								found = None
 
@@ -96,25 +101,36 @@ class RegistrationListener(Thread):
 									cursor.execute(
 										"""select * from productKey where activationKey = %s""",
 										(key,))
-
+									print(key)
 									found = cursor.rowcount
 
+								print("inserting")
 								inserted = cursor.execute(
 									"""insert into productKey(activationKey, userID)
 									values(%s, %s)""", (key, userID))
 
-								cursor.commit()
-
+								db.commit()
+								print("commit")
 								if inserted != 1:
 									success = False
 									msg = 'Failed to register activation key, contact support.'
+								else:
+									mail = EmailMessage()
+									mail.set_content(key)
+									mail['Subject'] = 'Activation Key'
+									mail['From'] = 'benjmr96@gmail.com'
+									mail['To'] = email
+
+									s = smtplib.SMTP('localhost')
+									s.send_message(mail)
+									s.quit()
 							else:
 								success = False
 								msg += '\n\tFailed to register'
 
 				elif parts[0] == "ACTIVATE":
 					success = True
-					msg = 'Account Activated'
+					msg = ''
 
 					try:
 						key = parts[1]
@@ -127,7 +143,7 @@ class RegistrationListener(Thread):
 						cursor.execute(
 							"""select * from productKey
 							where activationKey = %s and userID = %s""", (key, userID))
-
+						print("selected")
 						if cursor.rowcount != 1:
 							success = False
 							msg = 'Key not registered to user.'
@@ -136,13 +152,18 @@ class RegistrationListener(Thread):
 								"""update productKey
 								set activationCount = activationCount + 1
 								where activationKey = %s and userID = %s""", (key, userID))
-				else:	
+							db.commit()
+				else:
 					success = False
 					msg = 'Invalid Instruction'
 
-				self.socket.send_string(str(success) + '  ' + msg + '  ' + str(usedID))
-			except:
-				pass
+				if success:
+					msg = '\n\tAccount created and activation key registered, check your email'
+
+				self.socket.send_string(str(success) + '  ' + msg + '  ' + str(userID))
+
+			except Exception as e:
+				print(e)
 
 		self.monitor.stop = True
 		self.socket.disable_monitor()
