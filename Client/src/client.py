17367d17d1b2435d7d1b6f62fe6f8a52ad7e2196
Ben Ryan <ben96ryan@gmail.com>
2019-03-10 13:03:34 +0000

    fixed error message display bug, wrapped validation functions in a class

--- Client/src/client.py
+++ Client/src/client.py
@@ -1,5 +1,6 @@
 # TODO
 
+import os
 import sys
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -15,6 +16,8 @@ from networker import GlobalContextHandler, GlobalCertificateHandler
 import zmq
 import string
 
+from validator import Validator
+
 serverAddr = 'tcp://35.204.135.105'
 localAddr = 'tcp://127.0.0.1'
 registrationPort = ':5001'
@@ -22,13 +25,26 @@ registrationPort = ':5001'
 
 class mainWindow(QMainWindow):
 	def __init__(self, app):
-		loginDialog = LoginDialog()
-		result = loginDialog.getUserData()
-		print(result)
 		QMainWindow.__init__(self)
 		self.app = app
-		tabs = MainWindowTabs(app)
-		self.setCentralWidget(tabs)
+
+		authenticated = False
+
+		if os.path.exists("../userDetails.config"):
+			authenticated = authenticateUserDetails()
+		else:
+			loginDialog = LoginDialog()
+			authenticated, msg, userID, key = loginDialog.getUserData()
+			
+			self.saveUserDetails(userID, key)
+
+		if authenticated:
+			tabs = MainWindowTabs(app)
+			self.setCentralWidget(tabs)
+		else:
+			self.closeEvent(None)
+
+	def saveUserDetails()
 
 	def closeEvent(self, event):
 		activeThreads = threading.enumerate()
@@ -47,7 +63,7 @@ class mainWindow(QMainWindow):
 		ctxHandler.cleanup()
 
 		self.app.exit()
-		# sys.exit()
+		sys.exit()
 
 
 class LoginDialog(QDialog):
@@ -63,14 +79,6 @@ class LoginDialog(QDialog):
 		outerLayout = QHBoxLayout()
 		outerLayout.addWidget(tabs)
 		self.setLayout(outerLayout)
-
-		self.lowercase = list(string.ascii_lowercase)
-		self.uppercase = list(string.ascii_uppercase)
-		self.digits = list(string.digits)
-		self.whitespace = list(string.whitespace)
-		self.special = list(string.punctuation)
-
-		self.allValid = list(string.printable)
 
 	def setupLoginTab(self):
 		loginTab = QWidget()
@@ -102,6 +110,7 @@ class LoginDialog(QDialog):
 		outerLoginLayout.addLayout(usernameLayout)
 		outerLoginLayout.addLayout(passwordLayout)
 		outerLoginLayout.addLayout(buttonLayout)
+		outerLoginLayout.addWidget(self.msgLabelLogin)
 		loginTab.setLayout(outerLoginLayout)
 
 		return loginTab
@@ -164,23 +173,20 @@ class LoginDialog(QDialog):
 		username = self.usernameEntryLogin.text()
 		password = self.passwordEntryLogin.text()
 
-		verified, msg = self.verifyUser(username, password)
-
-		if verified:
+		self.authenticated, self.msg = self.verifyUser(username, password)
+		print(self.msg)
+		if self.authenticated:
 			self.accept()
 		else:
-			self.msgLabelLogin.setText(msg)
-
-		self.accept()
+			self.msgLabelLogin.setText(self.msg)
 
 	def verifyUser(self, username, password):
-		authenticated = True
+		authenticated = False
 		msg = ''
 
 		socket = setupGlobalSocket(serverAddr + registrationPort)
 		socket.send_string('LOGIN' + ' ' + username + ' ' + password)
 		result = socket.recv_string()
-		print(result)
 		parts = result.split('  ')
 
 		if parts[0] == 'True':
@@ -188,126 +194,7 @@ class LoginDialog(QDialog):
 		else:
 			authenticated = False
 
-		return authenticated, msg
-
-	def validateUsername(self, username):
-		valid = True
-		msg = ''
-
-		if username is None or username == '':
-			valid = False
-			msg += '\n\tMust not be blank'
-
-		if len(username) < 5:
-			valid = False
-			msg += '\n\tMust be at least 5 characters long'
-		elif len(username) > 32:
-			valid = False
-			msg += '\n\tMust be no more than 32 characters long'
-
-		if any(char in username for char in self.whitespace):
-			valid = False
-			msg += '\n\tMust not contain whitespace'
-
-		if any(char in username for char in self.special):
-			valid = False
-			msg += '\n\tMust not contain speical characters'
-
-		for char in username:
-			if char not in self.allValid:
-				valid = False
-				msg = '\n\tInvalid character: ' + char
-				break
-
-		return valid, msg
-
-	def validatePassword(self, password, passConf, username):
-		valid = True
-		msg = ''
-
-		if password is None or password == '':
-			valid = False
-			msg += '\n\tMust not be blank'
-
-		if password != passConf:
-			valid = False
-			msg += '\n\tConfirmation password does not match'
-
-		if len(password) < 8:
-			valid = False
-			msg += '\n\tMust be at least 8 characters long'
-
-		elif len(password) > 128:
-			valid = False
-			msg += '\n\tMust not be no more than 128 characters long'
-
-		if username in password:
-			valid = False
-			msg += '\n\tMust not contain username'
-
-		if not any(char in password for char in self.lowercase):
-			valid = False
-			msg += '\n\tMust contain at least one lowercase letter'
-
-		if not any(char in password for char in self.uppercase):
-			valid = False
-			msg += '\n\tMust contain at least one uppercase letter'
-
-		if not any(char in password for char in self.digits):
-			valid = False
-			msg += '\n\tMust contain at least one digit'
-
-		if not any(char in password for char in self.special):
-			valid = False
-			msg += '\n\tMust contain at least one special character'
-
-		if any(char in password for char in self.whitespace):
-			valid = False
-			msg += '\n\tMust not contain whitespace'
-
-		for char in password:
-			if char not in self.allValid:
-				valid = False
-				msg = '\n\tInvalid character: ' + char
-				break
-
-		return valid, msg
-
-	def validateEmail(self, email):
-		valid = True
-		msg = ''
-
-		if email is None or email == '':
-			valid = False
-			msg += '\n\tMust not be blank'
-		else:
-			parts = email.split('@')
-			if len(parts) != 2:
-				valid = False
-				msg += '\n\tInvalid structure regarding @'
-			else:
-				prefix = parts[0]
-				domain = parts[1]
-
-				if prefix == '' :
-					valid = False
-					msg += '\n\tBlank prefix'
-
-				if domain == '':
-					valid = False
-					msg += '\n\tBlank Domain'
-
-				parts = domain.split('.')
-
-				if len(parts) != 2 or parts[0] == '' or parts[1] == '':
-					valid = False
-					msg += '\n\tInvalid Domain'
-
-		if len(email) > 254:
-			valid = False
-			msg += "\n\tMust not exceed 254 characters"
-
-		return valid, msg
+		return authenticated, parts[1]
 
 	def register(self):
 		username = self.usernameEntryRegister.text()
@@ -318,19 +205,21 @@ class LoginDialog(QDialog):
 		validParameters = True
 		msg = ''
 
-		validUsername, usernameMsg = self.validateUsername(username)
+		validator = Validator()
+
+		validUsername, usernameMsg = validator.validateUsername(username)
 		if usernameMsg != '':
 			msg += '\nUsername:' + usernameMsg
 
 		# password validation uses username
 		if validUsername:
-			validPassword, passwordMsg = self.validatePassword(
+			validPassword, passwordMsg = validator.validatePassword(
 				password, passConf, username)
 
 			if passwordMsg != '':
 				msg += '\nPassword:' + passwordMsg
 
-		validEmail, emailMsg = self.validateEmail(email)
+		validEmail, emailMsg = validator.validateEmail(email)
 		if emailMsg != '':
 			msg += '\nEmail:' + emailMsg
 
@@ -348,9 +237,9 @@ class LoginDialog(QDialog):
 
 			if registered:
 				activationDialog = ActivationDialog()
-				result = activationDialog.getKey(userID)
-				if result:
-					self.msgLabelRegister.setText('Donezo')
+				self.authenticated, self.msg = activationDialog.getKey(userID)
+				if self.authenticated:
+					self.accept()
 				else:
 					self.msgLabelRegister.setText('Error please try again')
 			else:
@@ -381,7 +270,7 @@ class LoginDialog(QDialog):
 
 	def getUserData(self):
 		self.exec()
-		return self.result
+		return self.authenticated, self.msg
 
 	def closeEvent(self, event):
 		sys.exit()
@@ -436,7 +325,8 @@ class ActivationDialog(QDialog):
 
 	def activate(self):
 		key = self.activationEntryRegister.text()
-		validKey, msg = self.validateKey(key)
+		validator = Validator()
+		validKey, msg = validator.validateKey(key)
 
 		if validKey:
 			socket = setupGlobalSocket(serverAddr + registrationPort)
@@ -445,15 +335,14 @@ class ActivationDialog(QDialog):
 			parts = result.split('  ')
 
 			activated = parts[0]
-			msg = parts[1]
+			self.msg = parts[1]
 
 			if activated == 'True':
-				activated = True
+				self.authenticated = True
 			else:
-				activated = False
+				self.authenticated = False
 
-			if activated:
-				self.result = True
+			if self.authenticated:
 				self.accept()
 			else:
 				self.msgLabelActivate.setText(msg)
@@ -463,32 +352,12 @@ class ActivationDialog(QDialog):
 	def cancel(self):
 		self.reject()
 
-	def validateKey(self, key):
-		valid = True
-		msg = ''
-
-		hexdigits = list(string.hexdigits)
-
-		if key is None or key == '':
-			valid = False
-			msg += '\n\tMust not be blank'
-
-		if len(key) != 32:
-			valid = False
-			msg += '\n\tMust be exactly 32 characters long'
-
-		for char in key:
-			if char not in hexdigits:
-				valid = False
-				msg += '\n\tInvalid Character: ' + char
-				break
-
-		return valid, msg
+	
 
 	def getKey(self, userID):
 		self.userID = userID
 		self.exec()
-		return self.result
+		return self.authenticated, self.msg
 
 
 class MainWindowTabs(QTabWidget):
---
+++ Client/src/validator.py
@@ -0,0 +1,151 @@
+import string
+
+
+class Validator():
+    def __init__(self):
+        self.lowercase = list(string.ascii_lowercase)
+        self.uppercase = list(string.ascii_uppercase)
+        self.digits = list(string.digits)
+        self.whitespace = list(string.whitespace)
+        self.special = list(string.punctuation)
+        self.hexdigits = list(string.hexdigits)
+        self.allValid = list(string.printable)
+
+    def validateUsername(self, username):
+        valid = True
+        msg = ''
+
+        if username is None or username == '':
+            valid = False
+            msg += '\n\tMust not be blank'
+
+        if len(username) < 5:
+            valid = False
+            msg += '\n\tMust be at least 5 characters long'
+        elif len(username) > 32:
+            valid = False
+            msg += '\n\tMust be no more than 32 characters long'
+
+        if any(char in username for char in self.whitespace):
+            valid = False
+            msg += '\n\tMust not contain whitespace'
+
+        if any(char in username for char in self.special):
+            valid = False
+            msg += '\n\tMust not contain speical characters'
+
+        for char in username:
+            if char not in self.allValid:
+                valid = False
+                msg = '\n\tInvalid character: ' + char
+                break
+
+        return valid, msg
+
+    def validatePassword(self, password, passConf, username):
+        valid = True
+        msg = ''
+
+        if password is None or password == '':
+            valid = False
+            msg += '\n\tMust not be blank'
+
+        if password != passConf:
+            valid = False
+            msg += '\n\tConfirmation password does not match'
+
+        if len(password) < 8:
+            valid = False
+            msg += '\n\tMust be at least 8 characters long'
+
+        elif len(password) > 128:
+            valid = False
+            msg += '\n\tMust not be no more than 128 characters long'
+
+        if username in password:
+            valid = False
+            msg += '\n\tMust not contain username'
+
+        if not any(char in password for char in self.lowercase):
+            valid = False
+            msg += '\n\tMust contain at least one lowercase letter'
+
+        if not any(char in password for char in self.uppercase):
+            valid = False
+            msg += '\n\tMust contain at least one uppercase letter'
+
+        if not any(char in password for char in self.digits):
+            valid = False
+            msg += '\n\tMust contain at least one digit'
+
+        if not any(char in password for char in self.special):
+            valid = False
+            msg += '\n\tMust contain at least one special character'
+
+        if any(char in password for char in self.whitespace):
+            valid = False
+            msg += '\n\tMust not contain whitespace'
+
+        for char in password:
+            if char not in self.allValid:
+                valid = False
+                msg = '\n\tInvalid character: ' + char
+                break
+
+        return valid, msg
+
+    def validateEmail(self, email):
+        valid = True
+        msg = ''
+
+        if email is None or email == '':
+            valid = False
+            msg += '\n\tMust not be blank'
+        else:
+            parts = email.split('@')
+            if len(parts) != 2:
+                valid = False
+                msg += '\n\tInvalid structure regarding @'
+            else:
+                prefix = parts[0]
+                domain = parts[1]
+
+                if prefix == '' :
+                    valid = False
+                    msg += '\n\tBlank prefix'
+
+                if domain == '':
+                    valid = False
+                    msg += '\n\tBlank Domain'
+
+                parts = domain.split('.')
+
+                if len(parts) != 2 or parts[0] == '' or parts[1] == '':
+                    valid = False
+                    msg += '\n\tInvalid Domain'
+
+        if len(email) > 254:
+            valid = False
+            msg += "\n\tMust not exceed 254 characters"
+
+        return valid, msg
+
+    def validateKey(self, key):
+        valid = True
+        msg = ''
+
+        if key is None or key == '':
+            valid = False
+            msg += '\n\tMust not be blank'
+
+        if len(key) != 32:
+            valid = False
+            msg += '\n\tMust be exactly 32 characters long'
+
+        for char in key:
+            if char not in self.hexdigits:
+                valid = False
+                msg += '\n\tInvalid Character: ' + char
+                break
+
+        return valid, msg
