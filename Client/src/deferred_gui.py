normalized bounding box values for easy scaling, fixed bug where results array was being reversed(somehow), added notify run channel

Client/src/deferred_gui.py
@@ -1,31 +1,33 @@
 #TODO
 
-import numpy as np
-import cv2
-import sys
+import base64 as b64
+import copy
 import json
+import logging
 import math
+import os
+import shutil
+import sys
+import time
+from collections import deque
 from pprint import pprint
+from threading import Thread
+
+import cv2
+import ffmpeg
+import numpy as np
 from PySide2.QtCore import *
 from PySide2.QtGui import *
-from PySide2.QtWidgets import *
 from PySide2.QtMultimedia import *
 from PySide2.QtMultimediaWidgets import *
-from threading import Thread
-import time
-import logging
-from collections import deque
-import os
-from terminator import Terminator
-import copy
-from networker import Networker
-from feed_loader import FeedLoader
-import base64 as b64
-import ffmpeg
-import shutil
-from display_connector import DisplayConnector
-import data_handler
+from PySide2.QtWidgets import *
+
 import _pickle as pickle
+import data_handler
+from display_connector import DisplayConnector
+from feed_loader import FeedLoader
+from networker import Networker
+from terminator import Terminator
 
 
 class DeferredAnalysis(QWidget):
@@ -457,7 +459,7 @@ class Viewer(QWidget):
 
 				print(fps)
 				resultsFile = open(self.resultPath, 'rb')
-				resultsData = pickle.load(resultsFile)
+				resultsData = pickle.load(resultsFile)[::-1]
 				resultsFile.close()
 
 				print('V:', str(videoTotal), 'R:', len(resultsData))
@@ -480,22 +482,28 @@ class Viewer(QWidget):
 						frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 						if boundingBoxes != []:
+							height, width, _ = np.shape(frame)
 							for box in boundingBoxes:
-								x, y, w, h = int(box[0]*2), int(box[1]*2), int(box[2]*2), int(box[3]*2)
-								cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+								xf, yf, wf, hf = box
+								x = int(self.scale(xf, 0, 1, 0, width))
+								y = int(self.scale(yf, 0, 1, 0, height))
+								w = int(self.scale(wf, 0, 1, 0, width))
+								h = int(self.scale(hf, 0, 1, 0, height))
+
+								cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 						self.displayConn.emitFrame(frame)
 					else:
-						self.path = None
-						self.resultPath = None
-						self.ready = False
-						self.reset = True
-						break
+						feed.set(cv2.CAP_PROP_POS_FRAMES, 0)
+						self.frameCount = 0
 
 					self.frameCount += 1
 
 				print("break")
 				feed.release()
+		
+		def scale(self, val, inMin, inMax, outMin, outMax):
+			return ((val - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin
 
 		def set(self, contentType, name):
 			print("in set")

Client/src/networker.py
@@ -22,6 +22,7 @@ import _pickle as pickle
 import cv2
 import numpy as np
 from display_connector import DisplayConnector
+from notify_run import Notify
 
 
 class Networker(Thread):
@@ -126,6 +127,16 @@ class Networker(Thread):
 
 		results = []
 
+		try:
+			notify = Notify()
+			# notify.configure('https://notify.run/c/fA32tvPxqKTDmVMJ')
+			logging.debug('Browser Notification Endpoint Configured')
+		except:
+			notify = None
+			logging.critical('Failed to configure notify endpoint', exc_info=True)
+
+		notifyTimer = time.time()
+
 		while not terminator.isTerminating() and self.end is False or (self.end is True and len(self.frames) > 0):
 			if self.nextFrame is not None or len(self.frames) > 0:
 				self.total += 1
@@ -145,11 +156,26 @@ class Networker(Thread):
 
 				result = pickle.loads(serial)
 
+				if result[0] > 0.99 and not self.deferredMode:
+					if notify is not None and time.time() - notifyTimer >= 30:
+						notify.send(
+							'Weapon detected at level ' +
+							str(self.camera.levelID) + ' ' +
+							self.camera.location)
+						notifyTimer = time.time()
+					
+					# emit signal to gui for notification
+
 				boundingBoxes = result[1]
 
 				if boundingBoxes != []:
+					height, width, _ = np.shape(displayFrame)
 					for box in boundingBoxes:
-						x, y, w, h = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+						x, y, w, h = box
+						x = int(self.scale(x, 0, 1, 0, width))
+						y = int(self.scale(y, 0, 1, 0, height))
+						w = int(self.scale(w, 0, 1, 0, width))
+						h = int(self.scale(h, 0, 1, 0, height))
 
 						cv2.rectangle(displayFrame, (x,y), (x+w, y+h), (0,255,0), 2)
 
@@ -182,3 +208,6 @@ class Networker(Thread):
 			certHandler.cleanup()
 		except Exception as e:
 			print("exception while ending", e)
+
+	def scale(self, val, inMin, inMax, outMin, outMax):
+		return ((val - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin

Server/src/feed_handler.py
@@ -94,11 +94,11 @@ class FeedHandler(Thread):
 						regions, drawCoords = helper.extractRegions(frame, 3, (64, 64), True)
 						results = np.around(model.predict(regions)[:, 10:], decimals=3)
 						resultHandler.append(results)
-						alert = resultHandler.assess()
+						finalResult = resultHandler.assess()
 
 						boundingBoxes = bgRemover.apply(frame)
 
-						response = pickle.dumps((results, boundingBoxes), protocol=4)
+						response = pickle.dumps((finalResult, boundingBoxes), protocol=4)
 						self.socket.send(response)
 
 		self.socket.disable_monitor()

Server/src/feed_process_helper.py
@@ -100,8 +100,8 @@ class BackgroundRemover():
 		mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, self.openElement)
 		mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, self.closeElement)
 
-		h, w = np.shape(frame)
-		minArea = (h*w) * 0.0075
+		height, width = np.shape(frame)
+		minArea = (height*width) * 0.0075
 
 		contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		contours = sorted(contours, key=cv2.contourArea)
@@ -109,10 +109,17 @@ class BackgroundRemover():
 		boundingBoxes = []
 		for contour in contours:
 			if cv2.contourArea(contour) > minArea and len(boundingBoxes) < 5:
-				x, y, w, h = cv2.boundingRect(contour)
-				scaleVal = 2.5
-				boundingBoxes.append([x*scaleVal, y*scaleVal, w*scaleVal, h*scaleVal])
+				xf, yf, wf, hf = cv2.boundingRect(contour)
+				x = self.scale(xf, 0, width, 0, 1)
+				y = self.scale(yf, 0, height, 0, 1)
+				w = self.scale(wf, 0, width, 0, 1)
+				h = self.scale(hf, 0, height, 0, 1)
+
+				boundingBoxes.append([x, y, w, h])
 			else:
 				break
 
 		return boundingBoxes
+
+	def scale(self, val, inMin, inMax, outMin, outMax):
+		return ((val - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin